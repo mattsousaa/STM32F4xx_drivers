625d3fc3ca9b871023c164b58d6f6884c6684280
mattsousaa <mateuseng_ec@alu.ufc.br>
2020-05-26 16:59:26 -0300

    testing stm as slave

--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -4,16 +4,16 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/012i2c_slave_tx_string.c 
+../Src/013i2c_slave_tx_string2.c 
 
 OBJS += \
-./Src/012i2c_slave_tx_string.o 
+./Src/013i2c_slave_tx_string2.o 
 
 C_DEPS += \
-./Src/012i2c_slave_tx_string.d 
+./Src/013i2c_slave_tx_string2.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/012i2c_slave_tx_string.o: ../Src/012i2c_slave_tx_string.c
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F401RETx -DSTM32F4 -DDEBUG -c -I../Inc -I"C:/Users/mateu/Documents/Curso STM32/Projetos - STM32/STM32F4xx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/012i2c_slave_tx_string.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Src/013i2c_slave_tx_string2.o: ../Src/013i2c_slave_tx_string2.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F401RETx -DSTM32F4 -DDEBUG -c -I../Inc -I"C:/Users/mateu/Documents/Curso STM32/Projetos - STM32/STM32F4xx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/013i2c_slave_tx_string2.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
